f86dc1038dd0c9aec833e2288a3dd285e6363d1c
charlessommers <csommers@udel.edu>
2025-05-08 13:39:35 -0400

    Updated controller class to include new action and piece

--- src/Controller.ts
+++ src/Controller.ts
@@ -9,8 +9,10 @@ import { ActionRecruit } from "./game/actions/ActionRecruit";
 import { ActionSpawn } from "./game/actions/ActionSpawn";
 import { ActionCrack } from "./game/actions/ActionCrack";
 import { ActionAttack } from "./game/actions/ActionAttack";
+import { ActionRangedAttack } from "./game/actions/ActionRangedAttack";
 import { Team } from "./game/elements/Team";
 import { GameBoard } from "./game/elements/GameBoard";
+import { PieceChemist } from "./game/elements/PieceChemist";
 
 export class Controller {
     private game: GameS25;
@@ -22,8 +24,18 @@ export class Controller {
     /* Private Methods */
     private createGame(rows: number, cols: number): GameS25 {
         const gameBoard = new GameBoard(rows, cols);
-        const teamA = new Team("Red", [ new PieceBlueHen("Red"), new PieceMinion("Red"), new PieceScrat("Red") ]);
-        const teamB = new Team("Blue", [ new PieceBlueHen("Blue"), new PieceMinion("Blue"), new PieceScrat("Blue") ]);
+        const teamA = new Team("Red", [
+            new PieceBlueHen("Red"),
+            new PieceMinion("Red"),
+            new PieceScrat("Red"),
+            new PieceChemist("Red")
+        ]);
+        const teamB = new Team("Blue", [
+            new PieceBlueHen("Blue"),
+            new PieceMinion("Blue"),
+            new PieceScrat("Blue"),
+            new PieceChemist("Blue")
+        ]);
         return new GameS25(gameBoard, teamA, teamB, "Red");
     }
 
@@ -38,6 +50,7 @@ export class Controller {
 
         if (actionType === "move") action = new ActionMove(this.game, startLocation, endLocation);
         else if (actionType === "attack") action = new ActionAttack(this.game, startLocation, endLocation);
+        else if (actionType === "rangedattack") action = new ActionRangedAttack(this.game, startLocation, endLocation);
         else if (actionType === "recruit") action = new ActionRecruit(this.game, startLocation, endLocation);
         else if (actionType === "spawn") action = new ActionSpawn(this.game, startLocation, endLocation);
         else if (actionType === "crack") action = new ActionCrack(this.game, startLocation, endLocation);
